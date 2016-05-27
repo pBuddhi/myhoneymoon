efca35b6e3a3873fbfcf7868b5f61435de7ec24a
pbuddhi <buddhi9417@gmail.com>
2016-05-27 20:13:30 +0530

    first moved to index

--- dummyapp/urls.py
+++ dummyapp/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-	url(r'^first$',views.firstview,name='firstview'),
+	url(r'^$',views.firstview,name='firstview'),
    	url(r'^second$',views.secondview,name='secondview'),
  	
 
