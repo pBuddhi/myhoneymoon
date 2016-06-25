c8ad7e6404e513957c843866b0ecf8449ccfd85f
pBuddhi <buddhi9417@gmail.com>
2016-06-25 11:56:20 +0530

    Update server_settings.py

--- myhoneymoon/server_settings.py
+++ myhoneymoon/server_settings.py
@@ -169,8 +169,8 @@ LOGGING = {
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'varun@tveen.in'
-EMAIL_HOST_PASSWORD = 'django123*'
+EMAIL_HOST_USER = 'email id'
+EMAIL_HOST_PASSWORD = 'password*'
 EMAIL_PORT = 587
 
 SEND_BROKEN_LINK_EMAILS = True
